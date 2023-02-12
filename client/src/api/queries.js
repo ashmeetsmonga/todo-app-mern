9407c3924ce4c521cc5c98dbb00dae7364dcd5f6
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-02-12 23:32:42 +0530

    completed on click  functionality added

--- client/src/api/queries.js
+++ client/src/api/queries.js
@@ -17,3 +17,8 @@ export const deleteTodo = async (_id) => {
 	const { data } = await axios.delete(`http://localhost:5000/api/v1/${_id}`);
 	return data;
 };
+
+export const updateTodo = async (todo) => {
+	const { data } = await axios.patch(`http://localhost:5000/api/v1/${todo._id}`, todo);
+	return data;
+};
--- client/src/components/Todo.jsx
+++ client/src/components/Todo.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import { AiFillDelete } from "react-icons/ai";
 import { useMutation, useQueryClient } from "react-query";
-import { deleteTodo } from "../api/queries";
+import { deleteTodo, updateTodo } from "../api/queries";
 
 const Todo = ({ todo }) => {
 	const queryClient = useQueryClient();
-	const mutation = useMutation(deleteTodo, {
+	const deleteMutation = useMutation(deleteTodo, {
+		onSuccess: () => {
+			queryClient.invalidateQueries("todos");
+		},
+	});
+
+	const updateMutation = useMutation(updateTodo, {
 		onSuccess: () => {
 			queryClient.invalidateQueries("todos");
 		},
@@ -13,14 +19,19 @@ const Todo = ({ todo }) => {
 
 	return (
 		<div
-			className={`bg-gray-600 flex justify-between items-center py-3 px-4 w-3/5 rounded-2xl text-2xl tracking-widest`}
+			className={`bg-gray-600 flex justify-between items-center py-3 px-4 w-3/5 rounded-2xl text-2xl tracking-widest cursor-pointer`}
+			onClick={() => {
+				updateMutation.mutate({ ...todo, completed: !todo.completed });
+			}}
 		>
 			<div className={`${todo.completed ? "line-through text-gray-300" : "text-white"}`}>
 				{todo.name}
 			</div>
-			<button className='text-red-400' onClick={() => mutation.mutate(todo._id)}>
-				<AiFillDelete />
-			</button>
+			<div className='flex gap-2'>
+				<button className='text-red-400' onClick={() => deleteMutation.mutate(todo._id)}>
+					<AiFillDelete />
+				</button>
+			</div>
 		</div>
 	);
 };
