249a299a296e18e59acc92326d0aa55fd60299c8
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-02-14 15:53:11 +0530

    password hashed

--- server/controllers/authControllers.js
+++ server/controllers/authControllers.js
@@ -1,11 +1,19 @@
 const User = require("../db/models/User");
+const bcrypt = require("bcryptjs");
 
 const login = async (req, res) => {
 	res.send("Login route");
 };
 
 const register = async (req, res) => {
-	const user = await User.create(req.body);
+	const { name, email, password } = req.body;
+
+	if (!name || !email || !password) throw new Error();
+
+	const salt = await bcrypt.genSalt(10);
+	const hashedPassword = await bcrypt.hash(password, salt);
+
+	const user = await User.create({ name, email, password: hashedPassword });
 	res.status(200).json(user);
 };
 
