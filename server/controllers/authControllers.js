da104e1cdb5e7fef194fa0197b3a7d2585607cf7
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-02-15 12:31:06 +0530

    custom unauthorized error added

--- server/controllers/authControllers.js
+++ server/controllers/authControllers.js
@@ -1,15 +1,16 @@
 const User = require("../db/models/User");
 const { StatusCodes } = require("http-status-codes");
+const UnAuthorizedError = require("../errors/unAuthorizedError");
 
 const login = async (req, res) => {
 	const { email, password } = req.body;
 	if (!email || !password) throw new Error();
 
 	const user = await User.findOne({ email });
-	if (!user) throw new Error("Invalid credentials");
+	if (!user) throw new UnAuthorizedError("Invalid Credentials 1");
 
 	const isPasswordCorrect = await user.checkPassword(password);
-	if (!isPasswordCorrect) throw new Error("Invalid credentials 2");
+	if (!isPasswordCorrect) throw new UnAuthorizedError("Invalid credentials 2");
 
 	const token = user.createJWT();
 	res.status(StatusCodes.OK).json({ name: user.name, token });
